f4f7b9f852bccc0fe3c671325be7bfebb6436097
yhat <gentim977@gmail.com>
2025-02-21 00:47:51 -0500

    New habits system, bug with month stats days counting

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import main_page_view, day_view
+
+from .views import day_view, main_page_view, set_habits_view, stats_view
 
 urlpatterns = [
     path('', main_page_view, name='main_page'),
     path('day/<int:year>/<int:month>/<int:day>/', day_view, name='day_view'),
+    path('set-habits/', set_habits_view, name='set_habits'),
+    path('stats/', stats_view, name='stats_view'),
 ]
